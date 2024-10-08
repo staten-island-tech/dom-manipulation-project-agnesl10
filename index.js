b8dd2ffba5d522004a245ff171289a8347b5f7d0
agnesl10 <agnesl10@nycstudents.net>
2024-10-08 12:07:28 -0400

    werwer

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+function formAnswer() {
+  let form = document.querySelector(".submit");
+  console.log(form);
+  form.addEventListener("click", function (event) {
+    event.preventDefault();
+    console.log(event);
+  });
+}
+formAnswer();
