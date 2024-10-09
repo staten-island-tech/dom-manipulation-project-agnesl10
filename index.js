1080685d518798e0a89ca8b62de1d2e0917753ff
agnesl10 <agnesl10@nycstudents.net>
2024-10-09 12:28:05 -0400

    sdfsdf

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const DOMSelectors = {
+  container: document.querySelector(".container"),
+  header: document.querySelector("h1"),
+  card: document.querySelector(".card"),
+  cardHeader: document.querySelector(".card-header"),
+  form: document.querySelector(".form"),
+  button: document.querySelector(".submit"),
+};
+
 function formAnswer() {
   let form = document.querySelector(".submit");
   console.log(form);
