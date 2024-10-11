15174daa8675db0d30a4adca32401556ba0d0348
agnesl10 <agnesl10@nycstudents.net>
2024-10-11 12:28:32 -0400

    asdasd

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const DOMSelectors = {
   cardHeader: document.querySelectorAll(".card-header"),
   form: document.querySelectorAll(".form"),
   button: document.querySelectorAll(".btn"),
+  input: document.querySelector("#input"),
 };
 
 function formAnswer() {
-  let form = document.querySelectorAll(".form");
-  console.log(form);
-  form.addEventListener("submit", function (event) {
+  let button = document.querySelectorAll(".btn");
+  console.log(button);
+  button.addEventListener("click", function (event) {
     event.preventDefault();
     console.log(event.target);
   });
@@ -18,4 +19,10 @@ function formAnswer() {
 console.log("connected");
 formAnswer();
 
+// DOMSelectors.container.insertAdjacentHTML("beforebegin", 'ur html'); //takes two arguments: where and what we are putting it
+DOMSelectors.form.addEventListener("click", function () {
+  let input = DOMSelectors.input.value;
+  DOMSelectors.box.insertAdjacentHTML("beforeend", `<p> ${input}</p>`);
+  DOMSelectors.input.value = ""; //resets button
+});
 function createCard(card, cardTitle) {}
